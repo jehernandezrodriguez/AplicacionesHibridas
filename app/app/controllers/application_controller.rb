dc149f5a6566188f33d121ab0bce811d2e64151a
jehernandezrodriguez <jehernandezrodriguez@gmail.com>
2018-09-06 19:56:15 -0500

    add json token

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
- 
-  before_action :configure_devise_params, if: :devise_controller?
   
+  before_action :configure_devise_params, if: :devise_controller?
+
   def configure_devise_params
-      
-      devise_parameter_sanitizer.permit(:sign_up) do |user|
-        user.permit(:name, :email, :password, :password_confirmation)
-      end
+
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+
+      user.permit(:name, :email, :password, :password_confirmation)
+
+    end
+
   end
 end
--- app/app/controllers/images_controller.rb
+++ app/app/controllers/images_controller.rb
@@ -1,10 +1,11 @@
 class ImagesController < ApplicationController
-    
+
     before_action :set_image, only: [:show, :edit, :update, :destroy]
+    
     def index
         @images = Image.all            # variable de tipo array @images
     end
-
+    
     def new
         @image = Image.new
     end
@@ -13,29 +14,32 @@ class ImagesController < ApplicationController
         #render plain: params[:image].inspect
         @image = Image.new images_params
         @image.save
-        redirect_to @image #redirecciona a una nueva vista    
+
+        #redirect_to @image  #redirecciona a una nueva vista
     end
 
     def show
+        
     end
-   
+
     def edit
+        
     end
 
     def update
+         # buscamos nuestro id del objeto imagen
         @image.update images_params 
         redirect_to @image
     end
-    
-    
+
     def destroy
+        
         @image.destroy
         redirect_to images_path 
     end
-   
-  
 
-    
+
+
     private
 
     def images_params
@@ -45,6 +49,4 @@ class ImagesController < ApplicationController
     def set_image
         @image = Image.find params[:id]
     end
-   
-
 end
